414ab04dc9f7548a1ee9bf6c5ab8b8718db28d47
theChiChen <theChiChen@users.noreply.github.com>
2017-04-10 00:53:44 +0800

    Add different methods
    Add different methods

--- CheckSum.py
+++ CheckSum.py
@@ -4,13 +4,36 @@ import os
 
 def checksum():
     print('\r\n')
-    for root, dirs, files in os.walk(".\\"):
-        for f in files:
-            if (f.split('.')[1] == 'TXE' or f.split('.')[1] == 'txe' or f.split('.')[1] == 'ME' or f.split('.')[1] == 'me'):
-                fh = open(os.path.join(root, f), 'rb')
+
+#
+# Solution 1 : Including all subdirectories
+#
+#    for root, dirs, files in os.walk(".\\"):
+#        for f in files:
+#            try:
+#                if (f.split('.')[1] == 'rom' or f.split('.')[1] == 'txe' or f.split('.')[1] == 'ME' or f.split('.')[1] == 'me'):
+#    #            if (f.endswith(".rom") or f.endswith(".ROM")):
+#                    fh = open(os.path.join(root, f), 'rb')
+#                    checksum = sum(bytearray(fh.read()))
+#                    print('{} = 0x{}'.format(f, format(checksum, '02x').upper()))
+#                    fh.close()
+#            except (RuntimeError, TypeError, NameError, IndexError):
+#                pass
+
+#
+# Solution 2 : Only search the current directory
+#
+    for f in os.listdir( ".\\" ):
+        try:
+            if (f.split('.')[1] == 'rom' or f.split('.')[1] == 'txe' or f.split('.')[1] == 'ME' or f.split('.')[1] == 'me'):
+#            if (f.endswith(".rom") or f.endswith(".ROM")):
+                fh = open(f, 'rb')
                 checksum = sum(bytearray(fh.read()))
                 print('{} = 0x{}'.format(f, format(checksum, '02x').upper()))
                 fh.close()
+        except (RuntimeError, TypeError, NameError, IndexError):
+            pass
+
     print('\r\n')
     os.system("pause")
 
